b726c2fde0811f53914e5c35e2e6466c1d9a3100
Геннадий Сысоев <72642131+PainRUS@users.noreply.github.com>
2026-09-07 21:54:49 +0300

    Expose price color configuration from main UI

--- renderer/pages/Home.tsx
+++ renderer/pages/Home.tsx
@@ -1,11 +1,17 @@
 // import React from 'react';
 import PriceList from "../components/PriceList";
 import PriceUpdateStatus from "../components/PriceUpdateStatus";
+import PriceColorSettings from "../components/PriceColorSettings";
 
 export function Home() {
   return (
     <div className="relative flex flex-col h-full min-h-0">
-      <PriceUpdateStatus />
+      <div className="shrink-0 flex items-stretch">
+        <div className="flex-1 min-w-0">
+          <PriceUpdateStatus />
+        </div>
+        <PriceColorSettings />
+      </div>
       <div className="flex-1 min-h-0">
         <PriceList />
       </div>
