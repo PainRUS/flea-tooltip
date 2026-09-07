e855ad98ad4d95b97eb94d09a3cd3f1846311ca4
Геннадий Сысоев <72642131+PainRUS@users.noreply.github.com>
2026-09-07 03:05:49 +0300

    Enable UI translation in tooltip window

--- renderer/windows/tooltip.tsx
+++ renderer/windows/tooltip.tsx
@@ -1,10 +1,14 @@
-import { createRoot } from 'react-dom/client';
-import { Tooltip } from '../pages/Tooltip';
-import '../styles/index.css';
-import React from 'react';
+import { createRoot } from "react-dom/client";
+import { Tooltip } from "../pages/Tooltip";
+import UiTranslator from "../components/UiTranslator";
+import "../styles/index.css";
+import React from "react";
 
-const root = createRoot(
-  (document.getElementById("root") as Element)
-)
+const root = createRoot(document.getElementById("root") as Element);
 
-root.render(<Tooltip />);
+root.render(
+  <>
+    <UiTranslator />
+    <Tooltip />
+  </>
+);
