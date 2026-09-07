a44801c05467fdf45a8c61829648fb6914e45452
Геннадий Сысоев <72642131+PainRUS@users.noreply.github.com>
2026-09-07 03:05:42 +0300

    Enable UI translation in main window

--- renderer/pages/Layout.tsx
+++ renderer/pages/Layout.tsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
 import "../styles/index.css";
+import UiTranslator from "../components/UiTranslator";
 
 export function Layout() {
   return (
     <main className="p-2 pt-1 h-full">
+      <UiTranslator />
       <Outlet />
     </main>
   );
