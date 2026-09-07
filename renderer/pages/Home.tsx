2de53b6d7cf978692d5e6ed95b41ec038f486d5c
Геннадий Сысоев <72642131+PainRUS@users.noreply.github.com>
2026-09-07 23:09:39 +0300

    Expose quest tooltip settings

--- renderer/pages/Home.tsx
+++ renderer/pages/Home.tsx
@@ -2,6 +2,7 @@
 import PriceList from "../components/PriceList";
 import PriceUpdateStatus from "../components/PriceUpdateStatus";
 import PriceColorSettings from "../components/PriceColorSettings";
+import QuestTooltipSettings from "../components/QuestTooltipSettings";
 
 export function Home() {
   return (
@@ -10,6 +11,7 @@ export function Home() {
         <div className="flex-1 min-w-0">
           <PriceUpdateStatus />
         </div>
+        <QuestTooltipSettings />
         <PriceColorSettings />
       </div>
       <div className="flex-1 min-h-0">
